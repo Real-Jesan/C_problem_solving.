410f569fe9f0c164d88eff73503f72a22cb50ae8
Real-Jesan <jesan787898@gmail.com>
2026-05-19 08:16:06 +0600

    Change input method from fgets to gets
    Replaced fgets with gets for reading input.

---
+++ G_Conversion.c
@@ -0,0 +1,40 @@
+ #include<stdio.h>
+ #include<string.h>
+int main()
+{
+    char s[100000];
+    gets(s);
+    // int length = strlen(s);
+   
+
+    for(int i=0; s[i] != '\0'; i++)
+    {
+      if (s[i]==',')
+      {
+        s[i]= ' ';
+      }
+    
+
+       else if (s[i]>='A' && s[i]<='Z')
+      {
+        s[i]+=32;
+      }
+
+
+       else if (s[i]>='a' && s[i]<='z')
+      {
+        s[i]-=32;
+      }
+
+      
+      
+    }
+    
+    printf("%s",s);
+
+    return 0;
+}
+
+
+
+
