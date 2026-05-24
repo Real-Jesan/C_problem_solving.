d6a6bb2dd77fd0d050aaaaea2c20efceac290a44
Real-Jesan <jesan787898@gmail.com>
2026-05-24 21:08:57 +0600

    Fix loop to read array elements based on user input

---
+++ V_Frequency_Array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int arr[n];
+    for(int i=0; i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int fre[100001]={0};
+    for (int i=0; i<n; i++)
+    { 
+        // int val=arr[i];
+        // fre[val]++;   -> evabeo lekha jay.....
+
+        fre[arr[i]]++;
+        
+    }
+    
+    
+    for (int i=1; i<=m; i++)
+    {
+        printf("%d\n",fre[i]);
+    }
+
+    
+    return 0;
+}
