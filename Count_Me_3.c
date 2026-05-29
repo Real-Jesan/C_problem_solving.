0b6136e40867a4f95c496dec001cb159cbef9386
Real-Jesan <jesan787898@gmail.com>
2026-05-29 21:37:54 +0600

    Add character counting program in Count_Me_3.c
    Implement a program to count uppercase letters, lowercase letters, and digits in input strings.

---
+++ Count_Me_3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while (t--)
+    {
+      char s[10000];
+    scanf("%s",&s);
+    int a=0,b=0,c=0;
+    for(int i=0; s[i]!='\0';i++)
+    {
+        if(s[i]>='A' && s[i]<='Z' )
+        {
+            a++;
+
+        }
+
+         else if(s[i]>='a' && s[i]<='z' )
+         {
+            b++;
+         }
+        else if(s[i]>='0' && s[i]<='9' )
+        {
+            c++;
+        }
+    }
+     printf("%d %d %d\n",a,b,c);
+
+    }
+
+   
+    
+    return 0;
+}
